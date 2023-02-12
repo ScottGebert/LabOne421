33ecaf9e044d86c17fa9f81b4f91a461c36d37e6
Scott Gebert <scott22.gebert@gmail.com>
2023-02-11 23:40:53 -0700

    Added Timing for findHighPrices() for part b and c
    Just used System.nanoTime(); so not the most elegant implementation - if we just need times for the report then it's good enough, otherwise could make it a little prettier

--- src/project1/test.java
+++ src/project1/test.java
@@ -1,11 +1,28 @@
 package project1;
 
 public class test {
+  private static long startTime;
+  private static long endTime;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // Part B
+    startTime = System.nanoTime();
+    ShareInfo pricesStream = PickShareFunctional.findHighPrices(Shares.symbols.stream());
+    endTime = System.nanoTime();
+    printTime("findHighPrices time using stream: ");
+
+    // Part C
+    startTime = System.nanoTime();
+    ShareInfo pricesParallelStream = PickShareFunctional.findHighPrices(Shares.symbols.stream());
+    endTime = System.nanoTime();
+    printTime("findHighPrices time using parallel stream: ");
+
 		System.out.println(PickShareFunctional.findHighPrices(Shares.symbols.stream()));
 		System.out.println(PickShareFunctional.findHighPrices(Shares.symbols.parallelStream()));
 	}
+
+  private static void printTime(String messgae) {
+    System.out.println(messgae + ((endTime - startTime)) + "ns");
+  }
 
 }
